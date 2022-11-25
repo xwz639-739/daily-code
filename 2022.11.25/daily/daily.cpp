2cd67ff374d0dc2f462bf55e3100820fb889c1fb
xwz639-739 <xwz3448594810@163.com>
2022-11-25 13:49:09 +0800

    Signed-off-by: xwz639-739 <xwz3448594810@163.com> 从C到C++

---
+++ 2022.11.25/daily/daily.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+#include<string>
+
+//int main()
+//{
+//	int num = 0;
+//	int count = 0;
+//	cin >> num;
+//	while (num)
+//	{
+//		if (num % 2 == 1)
+//			count++;
+//		num /= 2;
+//	}
+//	cout << count << endl;
+//	system("pause");
+//	return 0;
+//}
+//求一个整数转化为二进制中1的个数
+
+//int main()
+//{
+//	int num, count=0, i;
+//	cin >> num;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (1 == ((num >> i) & 1))
+//		{
+//			count++;
+//		}
+//	}
+//	cout << count << endl;
+//	system("pause");
+//	return 0;
+//}
+//用位操作完成上一个操作
+
+//int main()
+//{
+//	int a = 10, b = 20, c = 30;
+//	int* arr[3] = { &a,&b,&c };
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cout << *arr[i] << endl;
+//	}
+//	system("pause");
+//	return 0;
+//}
+//创建指针数组及使用
