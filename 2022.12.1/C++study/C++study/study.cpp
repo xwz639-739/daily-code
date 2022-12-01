01ece0f240273fb6ce662ca4b737cd04a63c3374
xwz639-739 <xwz3448594810@163.com>
2022-12-01 15:55:45 +0800

    Signed-off-by: xwz639-739 <xwz3448594810@163.com> 写几个bug

---
+++ 2022.12.1/C++study/C++study/study.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+#include<string>
+
+//typedef class people
+//{
+//public:
+//	string name;
+//	int age=0;
+//	void Setname(string Name)
+//	{
+//		name = Name;
+//	}
+//	void Setage(int Age)
+//	{
+//		age = Age;
+//	}
+//	void Readname()
+//	{
+//		cout << "姓名：" << name << endl;
+//	}
+//	void Readage()
+//	{
+//		cout << "年龄：" << age << endl;
+//	}
+//	void Readlover()
+//	{
+//		cout << "对象是：" << lover << endl;
+//	}
+//	void Setpassword(int Password)
+//	{
+//		password = Password;
+//	}
+//private:
+//	string lover="don't know";
+//	int password=123456;
+//}p;
+//int main()
+//{
+//	p p1;
+//	p p2;
+//	cout << "请输入第一个人的名字：";
+//	string name1;
+//	cin >> name1;
+//	p1.Setname(name1);
+//	cout << "请输入第一个人的年龄：";
+//	int age1;
+//	cin >> age1;
+//	p1.Setage(age1);
+//	cout << "请输入第二个人的名字：";
+//	string name2;
+//	cin >> name2;
+//	p1.Setname(name2);
+//	cout << "请输入第二个人的年龄：";
+//	int age2;
+//	cin >> age2;
+//	p1.Setage(age2);
+//	cout << "对象1：";
+//	p1.Readlover();
+//	cout << "对象2：";
+//	p2.Readlover();
+//	cout << "修改第一个人的密码：";
+//	int password1;
+//	cin >> password1;
+//	p1.Setpassword(password1);
+//	cout << "修改第二个人的密码：";
+//	int password2;
+//	cin >> password2;
+//	p2.Setpassword(password2);
+//	cout << "第一个人的信息：";
+//	p1.Readname();
+//	p1.Readage();
+//	cout << "第二个人的信息：";
+//	p2.Readname();
+//	p2.Readage();
+//	system("pause");
+//	return 0;
+//}
