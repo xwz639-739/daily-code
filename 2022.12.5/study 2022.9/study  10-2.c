7f8a771dd29b3ce4033527cd7cf1344dafc41160
xwz639-739 <115339338+xwz639-739@users.noreply.github.com>
2022-12-05 16:40:10 +0800

    Update study  10-2.c

--- 2022.12.5/study 2022.9/study  10-2.c
+++ 2022.12.5/study 2022.9/study  10-2.c
@@ -2,53 +2,53 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
 
-//int my_strlen(char* str)
-//{
-//	if (*str != '\0')
-//		return 1+my_strlen(str+1);
-//	else
-//		return 0;
-//}
-//int main()
-//{
-//	char arr[] = "hello";
-//	int len = my_strlen(arr);
-//	printf("len=%d", len);
-//	return 0;
-//}
-//Çó×Ö·û´®µÄ³¤¶È
+int my_strlen(char* str)
+{
+	if (*str != '\0')
+		return 1+my_strlen(str+1);
+	else
+		return 0;
+}
+int main()
+{
+	char arr[] = "hello";
+	int len = my_strlen(arr);
+	printf("len=%d", len);
+	return 0;
+}
+//æ±‚å­—ç¬¦ä¸²çš„é•¿åº¦
 
-//int fac(int n)
-//{
-//	if (n == 1)
-//		return 1;
-//	else
-//		return n * fac(n - 1);
-//}
-//int main()
-//{
-//	int n = 0;
-//	int ret = 0;
-//	scanf("%d", &n);
-//	ret = fac(n);
-//	printf("%d", ret);
-//	return 0;
-//}
-//ÇónµÄ½×³Ë
+int fac(int n)
+{
+	if (n == 1)
+		return 1;
+	else
+		return n * fac(n - 1);
+}
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = fac(n);
+	printf("%d", ret);
+	return 0;
+}
+//æ±‚nçš„é˜¶ä¹˜
 
-//int fib(int n)
-//{
-//	if (n <= 2)
-//		return 1;
-//	else
-//		return fib(n - 1) + fib(n - 2);
-//}
-//int main()
-//{
-//	int n, ret;
-//	scanf("%d", &n);
-//	ret = fib(n);
-//	printf("ret=%d\n", ret);
-//	return 0;
-//}
-//ì³²¨ÄÇÆõÊýÁÐ
+int fib(int n)
+{
+	if (n <= 2)
+		return 1;
+	else
+		return fib(n - 1) + fib(n - 2);
+}
+int main()
+{
+	int n, ret;
+	scanf("%d", &n);
+	ret = fib(n);
+	printf("ret=%d\n", ret);
+	return 0;
+}
+//æ–æ³¢é‚£å¥‘æ•°åˆ—
