214818718e654cc78d541a2d7bd24f6b49c6d673
xwz639-739 <115339338+xwz639-739@users.noreply.github.com>
2022-12-05 16:42:31 +0800

    Update syudy  9-21.c

--- 2022.12.5/study 2022.9/syudy  9-21.c
+++ 2022.12.5/study 2022.9/syudy  9-21.c
@@ -1,11 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-//int main()
-//{
-//    char  ch=0;
-//	scanf("%c",&ch);
-//	printf("%d",ch);
-//	return 0;
-//}
-//×Ö·û´®µÄASCIIÂëÖµ
+int main()
+{
+   char  ch=0;
+	scanf("%c",&ch);
+	printf("%d",ch);
+	return 0;
+}
+//æ‰“å°ASCIIå€¼
